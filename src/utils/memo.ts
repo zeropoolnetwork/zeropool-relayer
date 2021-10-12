edaed4ae51c1b3bee37b1be40b9cc449cb74c722
Leonid <lenya_tyurin@mail.ru>
2021-10-12 12:57:25 +0300

    Extract memo constants

--- src/utils/memo.ts
+++ src/utils/memo.ts
@@ -1,8 +1,15 @@
 import fs from 'fs'
 
+import BN from 'bn.js'
 import { deserialize, BinaryReader } from 'borsh'
 import { TxType } from './helpers'
 import { logger } from '../services/appLogger'
+
+// Size in bytes
+const U256_SIZE = 32
+const POLY_1305_TAG_SIZE = 16
+const ACCOUNT_SIZE = 64
+const NOTE_SIZE = 60
 
 class Assignable {
   constructor(properties: Object) {
@@ -20,23 +27,22 @@ export class Memo extends Assignable {
   noteHashes!: Buffer[]
   rawNoteHashes!: Buffer
   a_p_x!: number
-  fee = null
-  amount = null
-  address = null
+  fee!: BN | null
+  amount!: BN | null
+  address!: Uint8Array | null
 }
 
 function memoBorshSchema(numNotes = 127) {
-  const POLY_1305_TAG_SIZE = 16
   const fields = [
-    ['accHash', [32]],
-    ['rawNoteHashes', [numNotes * 32]],
+    ['accHash', [U256_SIZE]],
+    ['rawNoteHashes', [numNotes * U256_SIZE]],
     ['a_p_x', 'u256'],
-    ['sharedSecretCiphertext', [(numNotes + 1) * 32 + POLY_1305_TAG_SIZE]],
-    ['accountCiphertext', [64 + POLY_1305_TAG_SIZE]],
+    ['sharedSecretCiphertext', [(numNotes + 1) * U256_SIZE + POLY_1305_TAG_SIZE]],
+    ['accountCiphertext', [ACCOUNT_SIZE + POLY_1305_TAG_SIZE]],
   ]
   for (let i = 0; i < numNotes; i++) {
     fields.push([`a_${i}_x`, 'u256'])
-    fields.push([`noteCiphertext_${i}`, [60 + POLY_1305_TAG_SIZE]])
+    fields.push([`noteCiphertext_${i}`, [NOTE_SIZE + POLY_1305_TAG_SIZE]])
   }
   return new Map([[Memo, {
     kind: 'struct',
@@ -47,7 +53,9 @@ function memoBorshSchema(numNotes = 127) {
 function splitHashes(rawHashes: Buffer, num: number): Buffer[] {
   const notes = []
   for (let i = 0; i < num; i++) {
-    const note_hash = Buffer.from(rawHashes.slice(i * 32, (i + 1) * 32))
+    const start = i * U256_SIZE
+    const end = start + U256_SIZE
+    const note_hash = Buffer.from(rawHashes.slice(start, end))
     notes.push(note_hash)
   }
   return notes
@@ -56,11 +64,14 @@ function splitHashes(rawHashes: Buffer, num: number): Buffer[] {
 export function decodeMemo(data: Buffer, txType: TxType | null) {
   logger.debug('Decoding memo...')
   const reader = new BinaryReader(data)
+  let fee = null
+  let amount = null
+  let addres = null
   if (txType) {
-    const fee = reader.readU64()
+    fee = reader.readU64()
     if (txType === TxType.WITHDRAWAL) {
-      const amount = reader.readU64()
-      const addres = reader.readFixedArray(20)
+      amount = reader.readU64()
+      addres = reader.readFixedArray(20)
     }
   }
   const numItems = new DataView(reader.readFixedArray(4).buffer).getUint32(0, true)
@@ -68,6 +79,9 @@ export function decodeMemo(data: Buffer, txType: TxType | null) {
   memo.numItems = numItems
   memo.noteHashes = splitHashes(memo.rawNoteHashes, numItems - 1)
   memo.rawBuf = data
+  memo.fee = fee
+  memo.amount = amount
+  memo.address = addres
   fs.writeFileSync('./data.json', JSON.stringify(memo, null, 2), 'utf-8');
   return memo
 }
