397fb90926ce66d573bfdc59ae7b316139f96002
Leonid <lenya_tyurin@mail.ru>
2022-07-21 13:32:48 +0300

    Add fee endpoint

--- zp-relayer/index.ts
+++ zp-relayer/index.ts
@@ -2,11 +2,11 @@ import './env'
 import fs from 'fs'
 import cors from 'cors'
 import express from 'express'
+import router from './router'
 import { logger } from './services/appLogger'
 import { createLoggerMiddleware } from './services/loggerMiddleware'
 import { createPoolTxWorker } from './poolTxWorker'
 import { createSentTxWorker } from './sentTxWorker'
-import endpoints from './endpoints'
 import { config } from './config/config'
 import { pool } from './pool'
 
@@ -27,19 +27,6 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.text())
-
-const router = express.Router()
-
-// Used only for testing as proving on client is now slow
-router.post('/proof_tx', endpoints.txProof)
-
-router.post('/sendTransaction', endpoints.sendTransaction)
-router.post('/sendTransactions', endpoints.sendTransactions)
-router.get('/transactions', endpoints.getTransactions)
-router.get('/transactions/v2', endpoints.getTransactionsV2)
-router.get('/merkle/root/:index?', endpoints.merkleRoot)
-router.get('/job/:id', endpoints.getJob)
-router.get('/info', endpoints.relayerInfo)
 
 app.use(createLoggerMiddleware('zp.log'))
 
