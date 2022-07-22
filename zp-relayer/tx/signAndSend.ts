854dc703961e5c1fd4a921de062e366ce595a9f9
Leonid <lenya_tyurin@mail.ru>
2022-07-22 16:50:07 +0300

    Fix sendTx; Update relayer to 1.1.14

--- zp-relayer/tx/signAndSend.ts
+++ zp-relayer/tx/signAndSend.ts
@@ -1,23 +1,8 @@
 import Web3 from 'web3'
 import type { TransactionConfig } from 'web3-core'
 
-export async function signAndSend(
-  { data, nonce, gasPrice, value, gas, to, chainId }: TransactionConfig,
-  privateKey: string,
-  web3: Web3
-): Promise<string> {
-  const serializedTx = await web3.eth.accounts.signTransaction(
-    {
-      nonce,
-      chainId,
-      to,
-      data,
-      value,
-      gasPrice,
-      gas,
-    },
-    privateKey
-  )
+export async function signAndSend(txConfig: TransactionConfig, privateKey: string, web3: Web3): Promise<string> {
+  const serializedTx = await web3.eth.accounts.signTransaction(txConfig, privateKey)
 
   return new Promise((res, rej) =>
     web3.eth
